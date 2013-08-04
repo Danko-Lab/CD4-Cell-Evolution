92918b0eb792b04f175ecd32275dc4ec9705bd36
dankoc <dankoc@gmail.com>
2013-08-04 15:41:19 -0400

    Minor bugfixes

--- writeDendrogram.R
+++ writeDendrogram.R
@@ -6,11 +6,11 @@ require(cluster)
 ## Create a dendrogram.
 ## Using more efficient counting/ merging using bedops and bash scripts.						
 ca <- read.table("countall.tsv")
-names(ca) <- c("chrom", "chromStart", "chromEnd", "name", "score", "strand", "type", "mapSize", "mgi", "Jurkat ", "Human 1", "Human 3", "Chimp 3", "R. Macaque 1", "R. Macaque 2")
-ca <- ca[!is.na(ca$V9),]
+names(ca) <- c("chrom", "chromStart", "chromEnd", "name", "score", "strand", "type", "mgi", "mapSize", "Jurkat ", "Human 1", "Human 3", "Chimp 3", "R. Macaque 1", "R. Macaque 2")
+ca <- ca[!is.na(ca[,10]),]
 count_df <- as.matrix(ca[,c(10:15)])/(ca[,9]) ## Normalize counts ...
 
-indx <- (ca$chromEnd-ca$chromStart)>50000
+indx <- (ca$mapSize)>50000
 
 cor(count_df, method="spearman")
 cor(count_df[indx,], method="spearman")
