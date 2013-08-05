a70a799aea66a6723f1330916c3c707c5253e758
dankoc <dankoc@gmail.com>
2013-08-04 21:47:33 -0400

    Draws scatterplots.

--- writeDendrogram.R
+++ writeDendrogram.R
@@ -1,6 +1,5 @@
 ## Get counts across species.
 ## 
-
 require(cluster)
 
 ## Create a dendrogram.
@@ -8,7 +7,8 @@ require(cluster)
 ca <- read.table("countall.tsv")
 names(ca) <- c("chrom", "chromStart", "chromEnd", "name", "score", "strand", "type", "mgi", "mapSize", "Jurkat ", "Human 1", "Human 3", "Chimp 3", "R. Macaque 1", "R. Macaque 2")
 ca <- ca[!is.na(ca[,10]),]
-count_df <- as.matrix(ca[,c(10:15)])/(ca[,9]) ## Normalize counts ...
+count_df <- as.matrix(ca[,c(10:15)])/(ca[,9]) #/ (colSums(ca[,c(10:15)])) ## Normalize counts ... RPKM
+for(i in 1:NCOL(count_df)) count_df[,i] <- count_df[,i]/sum(count_df[,i])
 
 indx <- (ca$mapSize)>50000
 
@@ -19,6 +19,16 @@ cor(count_df[indx,], method="spearman")
 clu <- agnes((1-cor(count_df[indx,], method="spearman")), diss=TRUE, method="ward")
 
 pdf("Agg.Hier.pdf")
-  plot(clu)
+ plot(clu)
+dev.off()
+
+require(hexbin)
+bin <- hexbin(x= log(count_df[,2]+1,10), y=log(count_df[,3]+1,10), 50)
+
+indx <- rowSums(count_df) > 0
+ 
+png("scatterplots.png", width = 1000, height = 1000)
+ hexplom(log(count_df[indx,]+min(count_df[count_df>0]),10), xbins=25, colramp = magent)
+ #pairs(log(count_df,10), pch=19)
 dev.off()
 
