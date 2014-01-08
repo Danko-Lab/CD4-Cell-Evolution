421809107e4a4a7473565c4d4b69da71aabf22af
Charles Danko <dankoc@gmail.com>
2014-01-07 21:41:45 -0500

    Added testSpec.bsh

---
+++ scan_H.sh
@@ -0,0 +1,48 @@
+#$ -S /bin/bash
+#$ -cwd
+#$ -N dREG.human
+#$ -o dREG.human.out.$JOB_ID
+#$ -j y
+#$ -pe bscb 32
+#$ -M dankoc@gmail.com
+#$ -m be
+#$ -l h_rt=24:00:00
+
+PREFIX=H
+DATADIR=All_Merge
+TH=0.83
+
+## Copy files to scratch space (/workdir and /SSD).
+STARTDIR=`pwd`
+SCRATCH=/SSD/cgd24_tssDetector_$PREFIX
+mkdir $SCRATCH
+cp $STARTDIR/scan_nhp.R $SCRATCH ## 
+cp /home/cgd24/projects/tss_detector/train_svm/asvm.RData $SCRATCH ## 
+cp ~/nextgen/projects/GROseq/NHP/$DATADIR/$PREFIX*.bw $SCRATCH
+cd $SCRATCH
+
+## Run R.
+R --no-save --args $PREFIX < scan_nhp.R
+
+## Merge into TSS. 
+for i in `ls *.TSS.bedGraph.gz`
+do
+  j=`echo $i | cut -d \. -f 1,2`
+  echo $j
+
+  ## Merge into peaks.
+  zcat $i | awk 'BEGIN{OFS="\t"} ($4 > '"$TH"') {print $1,$2-50,$3+51}' | sort-bed - | bedops --merge - > $j.no_score.bed
+
+  ## Get max score in each peak. 
+  zcat $i | awk 'BEGIN{OFS="\t"} {print $1,$2,$3,"n",$4}' | sort-bed - | bedmap --echo --max $j.no_score.bed - | sed "s/|/\t/g" | awk 'BEGIN{OFS="\t"} {print $1,$2,$3,"n",$4}' | gzip > $j.bed.gz
+
+  ## Clean temporary files.  
+  rm $j.no_score.bed
+done
+
+## Copy data files back.
+cp *.TSS.bedGraph.gz $STARTDIR
+cp *.bed.gz $STARTDIR
+
+## Cleanup
+rm -Rf $SCRATCH
---
+++ scan_nhp.R
@@ -0,0 +1,25 @@
+require(featureDetector)
+
+args <- commandArgs(trailingOnly=TRUE)
+prefix   <- args[1]
+
+
+## Read PRO-seq data.
+gs_plus_path  <- paste(prefix, c("-U_plus.bw","-PI_plus.bw"),sep="") #-U.bed.gz_plus.bw", "-PI.bed.gz_plus.bw"), sep="")
+gs_minus_path <- paste(prefix, c("-U_minus.bw","-PI_minus.bw"),sep="") #-U.bed.gz_minus.bw", "-PI.bed.gz_minus.bw"), sep="")
+
+outnames <- paste(prefix, c("-U.TSS.bedGraph.gz", "-PI.TSS.bedGraph.gz"), sep="")
+
+load("asvm.RData")
+gdm <- genomic_data_model(window_sizes= c(10, 25, 50, 500, 5000), half_nWindows= c(10, 10, 30, 20, 20)) 
+
+for(i in 1:length(gs_plus_path)) {
+	## Now scan all positions in the genome ...
+	inf_positions <- get_informative_positions(gs_plus_path[i], gs_minus_path[i], depth= 0, step=50, use_ANDOR=TRUE, use_OR=FALSE) ## Get informative positions.
+
+	pred_val<- eval_reg_svm(gdm, asvm, inf_positions, gs_plus_path[i], gs_minus_path[i], batch_size= 10000, ncores=62)
+
+	final_data <- data.frame(inf_positions, pred_val)
+	options("scipen"=100, "digits"=4)
+	write.table(final_data, file=pipe(paste("gzip -c > ",outnames[i])), row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+}
