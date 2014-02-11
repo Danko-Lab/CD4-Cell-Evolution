85cc2f1ed8e313bf8baff97d220c3adf3e743c31
Charles Danko <dankoc@gmail.com>
2014-02-11 12:28:21 -0500

    Checking in script to compare H2 and H2.2 PI.

---
+++ compareH2_H2.2_PI.R
@@ -0,0 +1,9 @@
+##
+##
+## compareH2_H2.2_PI.R -- Compares two independent draws of human 2.  
+##                        Differences: 
+##                           (1) Completely separate days for the prep and PRO-seq.
+##                           (2) H2 processed immediately, H2.2 processed after 24h at 4C.
+##                           (3) ...
+
+
