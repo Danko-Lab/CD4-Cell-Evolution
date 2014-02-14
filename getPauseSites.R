06d73cb0506f562b10684365d8e1a1cd2275c4cf
Charles Danko <dankoc@gmail.com>
2014-02-13 21:39:07 -0500

    Many updates to getPauseSites.R

--- getPauseSites.R
+++ getPauseSites.R
@@ -8,6 +8,8 @@
 
 require(bigWig)
 
+readThresh <- 10 ## Mainly want to eliminate passing position bed[1,i] for regions without any read density.
+
 size=100
 overlap=5
 nwindows <- size/overlap
@@ -25,6 +27,8 @@ bw_plus <- load.bigWig(bw_plus_file)
 bw_minus<- load.bigWig(bw_minus_file)
 
 ## Get the start position of 100bp window with the max counts inside each dREG site.
+chromPlus <- character()
+chromMinus<- character()
 startPlus <- integer()
 startMinus <- integer()
 
@@ -40,12 +44,18 @@ for(i in 1:NROW(bed)) {
   countsm_SUM <- sapply(1:(length(countsm)-nwindows), function(x) {sum(countsm[x:(x+nwindows)])})
   stopifnot(NROW(bases)-1-nwindows == NROW(countsm_SUM)) ## SANTIY CHECK
 
-  startPlus  <- c(startPlus, bases[1]+(which.max(countsp_SUM)-1)*overlap)
-  startMinus <- c(startMinus, bases[1]+(which.max(countsm_SUM)-1)*overlap)
+  if(max(countsp_SUM) > readThresh) {
+    startPlus  <- c(startPlus, bases[1]+(which.max(countsp_SUM)-1)*overlap)
+    chromPlus  <- c(chromPlus, as.character(bed[i,1]))
+  }
+  if(max(countsm_SUM) > readThresh) {
+    startMinus <- c(startMinus, bases[1]+(which.max(countsm_SUM)-1)*overlap)
+    chromMinus <- c(chromMinus, as.character(bed[i,1]))
+  }
 }
 
-data <- rbind(data.frame(chrom= bed[,1], start= startPlus, end= startPlus+size, name= bed[,4], score= bed[,5], strand= "+"),
-		data.frame(chrom= bed[,1], start= startMinus, end= startMinus+size, name= bed[,4], score= bed[,5], strand= "-"))
+data <- rbind(data.frame(chrom= chromPlus, start= startPlus, end= startPlus+size, name= ".", score= "0", strand= "+"),
+		data.frame(chrom= chromMinus, start= startMinus, end= startMinus+size, name= ".", score= "0", strand= "-"))
 
 options("scipen"=100, "digits"=4)
 write.table(data, pipe(paste("sort-bed - >", out_file)), sep="\t", col.names=FALSE, row.names=FALSE, quote=FALSE)
