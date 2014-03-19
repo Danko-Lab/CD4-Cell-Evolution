255fb712141a3ef78b37c63c2c0c3f1ebb7d59df
Charles Danko <dankoc@gmail.com>
2014-03-19 10:38:46 -0400

    Changed runLimmaQuantile to properly order by rownames

--- densScatterplot.R
+++ densScatterplot.R
@@ -1,7 +1,7 @@
 
 ## Use densCols() output to get density at each point.
 ## Thanks to StackOverflow for the quicky R lift: http://stackoverflow.com/questions/17093935/r-scatter-plot-symbol-color-represents-number-of-overlapping-points
-densScatterplot <- function(x1, x2, ...) {
+densScatterplot <- function(x1, x2, n=256, ...) {
   df <- data.frame(x1, x2)
 
   x <- densCols(x1,x2, colramp=colorRampPalette(c("black", "white")))
@@ -9,7 +9,7 @@ densScatterplot <- function(x1, x2, ...) {
 
   ## Map densities to colors
 #  cols <-  colorRampPalette(c("#000099", "#00FEFF", "#45FE4F", "#FCFF00", "#FF9400", "#FF3100"))(256)
-  cols <- colorRampPalette(c("light gray", "#000099", "#45FE4F", "#FCFF00", "#FF9400", "#FF3100"))(128)
+  cols <- colorRampPalette(c("light gray", "#000099", "#45FE4F", "#FCFF00", "#FF9400", "#FF3100"))(n)
   df$col <- cols[df$dens]
 
   ## Plot it, reordering rows so that densest points are plotted on top
