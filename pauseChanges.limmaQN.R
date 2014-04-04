a89c96a8a557c7dc79a57a237e337a86b7348419
Charles Danko <dankoc@gmail.com>
2014-04-03 22:02:37 -0400

    pauseChanges now registers genes with pause sites

--- pauseChanges.limmaQN.R
+++ pauseChanges.limmaQN.R
@@ -21,8 +21,19 @@ source("../lib/runLimmaQuantile.R")
 conditions <- c("Human", "Human", "Human", rep("NHP",5))#"Chimp", "Chimp", "RM", "RM", "RM")
 
 ## Use Limma to ID changes.
-hg <- runLimmaQuantile(gb[,c(12:14,16:20)-1], conditions, gb[,1:8], condA="Human", condB="NHP")
-hp <- runLimmaQuantile(ps[,c(12:14,16:20)-1], conditions, ps[,1:8], condA="Human", condB="NHP")
+hg <- runLimmaQuantile(gb[,c(12:14,16:20)-1], conditions, gb[,1:8], condA="Human", condB="NHP", q.cut=0.05)
+hp <- runLimmaQuantile(ps[,c(12:14,16:20)-1], conditions, ps[,1:8], condA="Human", condB="NHP", q.cut=0.05)
 
+#### NEW
+## Get ps and gb into the same ...
+
+body <-  hg$tab[hg$tab[,4] %in% hp$tab[,4],] # gb[gb[,4] %in% ps[,4],]
+pause <- hp$tab[match(as.character(body[,4]), as.character(hp$tab[,4])),] # ps[match(as.character(body[,4]), as.character(ps[,4])),]
+stopifnot(sum(body[,4] == as.character(pause[,4])) == NROW(pause)) ## SANTIY CHECK.
+
+
+
+indx <- pause[,"Human 1"]>0 & pause[,"Chimp 4"]>0 & pause[,"R. Macaque 3"]>0 ## Unnecesarily conservative.
+PI <- (((pause[,c(11:NCOL(pause))]+1)/pause[,"mapSize"]) / ((body[,c(11:NCOL(body))]+1)/body[,"mapSize"]))[indx,]
 
 
