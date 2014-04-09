3a02b2a427fdc6161b6a7fa4ba4d651129ca4b33
Charles Danko <dankoc@gmail.com>
2014-04-09 14:30:54 -0400

    PauseBodyCorrelation.

--- pauseChanges.limmaQN.R
+++ pauseChanges.limmaQN.R
@@ -3,6 +3,10 @@
 ## GOAL: Compare official changes in pause sites to changes in protein-coding genes.
 ##
 ##
+indxH <- c(11:13) #21:23 #12:13 -> Remove H1 (outlier for pause)
+indxC <- c(15:16) #25:26
+indxM <- c(17:19) #28:29
+indxHCM <- c(indxH, indxC, indxM)
 
 ## Read pause sites.
 ps <- read.table("../annotations/countpause.tsv")
@@ -12,31 +16,41 @@ ps <- ps[!is.na(ps[,10]),]
 gb <- read.table("../annotations/countall.tsv")
 gb <- gb[gb$V7 == "protein_coding" & !is.na(gb[,10]),]
 
-## Quantile normalize each independently.
-source("../lib/runLimmaQuantile.R")
-conditions <- c("Human", "Human", "Human", rep("NHP",5))#"Chimp", "Chimp", "RM", "RM", "RM")
+makePBPlot <- function(indx1, indx2, name1, name2, hv=3, lv=hv) {
+ indx <- c(indx1, indx2)
 
-## Use Limma to ID changes.
-hg <- runLimmaQuantile(gb[,c(12:14,16:20)-1], conditions, gb[,1:8], condA="Human", condB="NHP", q.cut=0.05)
-hp <- runLimmaQuantile(ps[,c(12:14,16:20)-1], conditions, ps[,1:8], condA="Human", condB="NHP", q.cut=0.05)
+ ## Quantile normalize each independently.
+ source("../lib/runLimmaQuantile.R")
+ conditions <- c(rep(name1, NROW(indx1)), rep(name2, NROW(indx2)))
+ 
+ ## Use Limma to ID changes.
+ hg <- runLimmaQuantile(gb[,indx], conditions, gb[,1:8], condA=name1, condB=name2, q.cut=0.05)
+ hp <- runLimmaQuantile(ps[,indx], conditions, ps[,1:8], condA=name1, condB=name2, q.cut=0.05)
+ 
+ ## Get ps and gb into the same indices, for the same genes ...
+ body <-  hg$tab[hg$tab[,4] %in% hp$tab[,4],] 
+ pause <- hp$tab[match(as.character(body[,4]), as.character(hp$tab[,4])),] 
+ stopifnot(sum(body[,4] == as.character(pause[,4])) == NROW(pause)) ## SANTIY CHECK.
+ 
+ source("../lib/densScatterplot.R")
+ #boxplot(body$logFC, pause$logFC, ylim=c(-3,3))
+ cc <- cor(body$logFC, pause$logFC, method="pearson")
+ print(paste("Correlation: ", cc, "Fraction of Varience: ", cc*cc))
+ #plot(body$logFC, pause$logFC)
+ densScatterplot(body$logFC, pause$logFC, xlab="Body", ylab="Pause", main=paste(name1,"vs",name2))
+ abline(v=c(hv,-hv), h=c(hv,-hv))
+ 
+ ## Human changes in pausing >3
+ ## # Pause / # Body
+ pc <- sum(abs(pause$logFC)>hv & abs(body$logFC)<lv)
+ bc <- sum(abs(body$logFC)>hv & abs(pause$logFC)<lv)
+ pv <- fisher.test(data.frame(c(pc, NROW(pause)), c(bc, NROW(body))))$p.value
+ print(paste("Fraction Pause_Q(2,8)/ Body_Q(4,6):", pc,"/",bc,"=", pc/ bc, "P=", pv))
+}
 
-## Get ps and gb into the same indices, for the same genes ...
-body <-  hg$tab[hg$tab[,4] %in% hp$tab[,4],] 
-pause <- hp$tab[match(as.character(body[,4]), as.character(hp$tab[,4])),] 
-stopifnot(sum(body[,4] == as.character(pause[,4])) == NROW(pause)) ## SANTIY CHECK.
-
-source("../lib/densScatterplot.R")
-#boxplot(body$logFC, pause$logFC, ylim=c(-3,3))
-cor.test(body$logFC, pause$logFC, method="spearman")
-#plot(body$logFC, pause$logFC)
-densScatterplot(body$logFC, pause$logFC, xlab="Body", ylab="Pause")
-abline(v=c(3,-3), h=c(3,-3))
-
-## Human changes in pausing >3
-hv <- 3; lv <- 0.5
-## # Pause / # Body
-sum(abs(pause$logFC)>hv & abs(body$logFC)<lv)/ sum(abs(body$logFC)>hv & abs(pause$logFC)<lv)
-
+makePBPlot(indxH, indxC, "Human", "Chimp", hv=5, lv=3)
+makePBPlot(indxH, indxM, "Human", "Macaque", hv=5, lv=3)
+makePBPlot(indxC, indxM, "Chimp", "Macaque", hv=5, lv=3)
 
 
 
