a52be6420c2265bb8ca31a2b31c9b730294da566
Charles Danko <dankoc@gmail.com>
2014-04-15 11:54:48 -0400

    Added script to compute ancestral TU given Andres species specific predictions

--- singleSpecies_hmm.R
+++ singleSpecies_hmm.R
@@ -318,14 +318,14 @@ decode_hmm <- function(chrom, hmm, full.data, train.data, missing.lst, step = 50
 
 # svn switch --relocate $SVN_ROOT/phast/trunk svn+ssh://cgd24@compgen.bscb.cornell.edu/usr/local/svnroot/phast/trunk .
  # Data: H1, C3, R2.
- Hp <- load.bigWig("/usr/projects/GROseq/CD4/Alignments/CD4-U_plus.bw")
- Hm <- load.bigWig("/usr/projects/GROseq/CD4/Alignments/CD4-U_minus.bw")
- Cp <- load.bigWig("/usr/projects/GROseq/NHP/Alignments_3rdPrep/C4-U.bed.gz_plus.hg19.bw")
- Cm <- load.bigWig("/usr/projects/GROseq/NHP/Alignments_3rdPrep/C4-U.bed.gz_minus.hg19.bw")
- Mp <- load.bigWig("/usr/projects/GROseq/NHP/Alignments_3rdPrep/M3-U.bed.gz_plus.hg19.bw")
- Mm <- load.bigWig("/usr/projects/GROseq/NHP/Alignments_3rdPrep/M3-U.bed.gz_minus.hg19.bw")
+ Hp <- load.bigWig("/usr/projects/GROseq/NHP/AllData/All_Merge/H-U_plus.bw")
+ Hm <- load.bigWig("/usr/projects/GROseq/NHP/AllData/All_Merge/H-U_minus.bw")
+# Cp <- load.bigWig("/usr/projects/GROseq/NHP/Alignments_3rdPrep/C4-U.bed.gz_plus.hg19.bw")
+# Cm <- load.bigWig("/usr/projects/GROseq/NHP/Alignments_3rdPrep/C4-U.bed.gz_minus.hg19.bw")
+# Mp <- load.bigWig("/usr/projects/GROseq/NHP/Alignments_3rdPrep/M3-U.bed.gz_plus.hg19.bw")
+# Mm <- load.bigWig("/usr/projects/GROseq/NHP/Alignments_3rdPrep/M3-U.bed.gz_minus.hg19.bw")
 
- all.data = make.data("chr22", list(Hp, Cp, Mp), list(Hm, Cm, Mm))
+ all.data = make.data("chr22", list(Hp), list(Hm))
 
  ## SANITY CHECK
  #head(t(all.data$data[1][[1]])) ## SANITY CHECK
