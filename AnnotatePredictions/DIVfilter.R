8b3371f345c4c7bce4e75de756c99e2a6b0a3a29
Charles Danko <dankoc@gmail.com>
2014-04-29 10:38:54 -0400

    AnnotationPipeline script moved into proper folder.  Some editing.

--- AnnotatePredictions/DIVfilter.R
+++ AnnotatePredictions/DIVfilter.R
@@ -1,4 +1,4 @@
-Do <- read.table("TMP/divergent.Overlap.bed")
-Dn <- read.table("TMP/divergent.NoOverlap.bed")
+Do <- read.table("GENCODE/divergent.Overlap.bed")
+Dn <- read.table("GENCODE/divergent.NoOverlap.bed")
 Dn <- Dn[!(Dn$V4 %in% Do$V4),]
-write.table(Dn, "TMP/divergent.NoOverlap.bed", row.names=F, quote=F, col.names=F, sep="\t")
+write.table(Dn, "GENCODE/divergent.NoOverlap.bed", row.names=F, quote=F, col.names=F, sep="\t")
