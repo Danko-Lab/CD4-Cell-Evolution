f2ea4e44ac3d617851d2ac1bd4b7d3263701f94b
Charles Danko <dankoc@gmail.com>
2014-05-09 10:13:10 -0400

    Adding R script to run PCA among pause site patterns

---
+++ pca_pause_sites.R
@@ -0,0 +1,13 @@
+#
+# Applies PCA to collect systematica differences between pause sites at 
+# orthologous promoters.
+
+## Get the 3' patterns at TSS.  Fiter for complete mappability.
+
+## PCA. Filter for pause sites that don't change in quantity.
+
+## Get pause sites whose PCA is significantly different across species.
+
+## Quantify frequency of shifts.  Look at types of shifts.  
+
+## Correlate shifts with specific patterns of TXN.
