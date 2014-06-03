b1b4b57a3558e0dd27d1f59433766d66d29e5546
Charles Danko <dankoc@gmail.com>
2014-06-03 15:48:05 -0400

    Final commit

--- dreg_scatterplots.R
+++ dreg_scatterplots.R
@@ -8,9 +8,9 @@ source("../lib/densScatterplot.R")
 a <- read.table("HCM-U-PI.dREG-tss-clusters.tsv")
 for(i in 7:12) a[is.na(a[,i]),i] <- 0 ## Assign 'NA' scores to 0.
 
-
+pdf("dREG.DensityScatterplot.pdf")
 densScatterplot(a$V7, a$V8, xlab="Human Unt.", ylab="Chimpanzee Unt.", main="dREG Scores")
 densScatterplot(a$V7, a$V9, xlab="Human Unt.", ylab="Rhesus Macaque Unt.", main="dREG Scores") 
 
 densScatterplot(a$V7, a$V10, xlab="Human Unt.", ylab="Human P/I", main="dREG Scores") 
-
+dev.off()
