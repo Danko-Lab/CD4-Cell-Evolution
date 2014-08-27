febc3ddb1a07986d7f6b7f0ff7abfe70b57111aa
Charles Danko <dankoc@gmail.com>
2014-08-27 12:59:36 -0400

    Improved ruv.R

--- annotations/ruv.R
+++ annotations/ruv.R
@@ -7,10 +7,12 @@ source("readData.R")
 require(ruv.DE)
 
 Y <- as.matrix(ca[,indx.good[2:17]])
-X <- as.integer(as.factor(rbind(c("H","H","H","C","C","M","M","M",    "H","H","H","C","C","M","M"), c(rep("U",8), rep("PI",7)))))
+#X <- as.integer(as.factor(rbind(c("H","H","H","C","C","M","M","M",    "H","H","H","C","C","M","M"), c(rep("U",8), rep("PI",7)))))
+
+X <- rbind(c(1,1,1,2,2,3,3,3,    1,1,1,2,2,3,3), c(rep(4,8), rep(5,7)))
 ctrl <- ca$type == "protein_coding" ## Assume no changes in protein-coding...
 
-norm <- RUVinv(Y, X, ctrl)
+norm_vals <- RUVinv(Y, X, ctrl)
 
 
 
