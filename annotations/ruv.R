01d03f1629f59ad42b6715c80006aed6b573a5f5
Charles Danko <dankoc@gmail.com>
2014-08-27 17:39:29 -0400

    More updates to ruv

--- annotations/ruv.R
+++ annotations/ruv.R
@@ -16,8 +16,13 @@ cd14m1 <- load.bigWig("/usr/data/GROseq.parser/hg19/cd14/rnaseq/wgEncodeCshlLong
 cd14m2 <- load.bigWig("/usr/data/GROseq.parser/hg19/cd14/rnaseq/wgEncodeCshlLongRnaSeqMonocd14CellPapMinusRep2.bigWig")
 
 ## Count reads ... 
-CD4c  <- bed.region.bpQuery.bigWig(cd4, ca[,1:6])
-CD14c <- bed.region.bpQuery.bigWig(cd14p1, ca[,1:6])+bed.region.bpQuery.bigWig(cd14p2, ca[,1:6])+bed.region.bpQuery.bigWig(cd14m1, ca[,1:6])+bed.region.bpQuery.bigWig(cd14m2, ca[,1:6])
+CD4c  <- bedQuery.bigWig(ca[,1:3], cd4, gapValue=0) #bed.region.bpQuery.bigWig(cd4, ca[,1:6])
+CD14c <- bedQuery.bigWig(ca[,1:3], cd14p1, gapValue=0)+ #+bed.region.bpQuery.bigWig(cd14p2, ca[,1:6])+bed.region.bpQuery.bigWig(cd14m1, ca[,1:6])+bed.region.bpQuery.bigWig(cd14m2, ca[,1:6])
+         bedQuery.bigWig(ca[,1:3], cd14p2, gapValue=0)+
+         bedQuery.bigWig(ca[,1:3], cd14m1, gapValue=0)+
+         bedQuery.bigWig(ca[,1:3], cd14m2, gapValue=0)
+
+ctrl <- (log((CD4c+1)/sum(CD4c))-log((CD14c+1)/sum(CD14c))) < -5
 
 ##
 ## Now apply the RUV method...
@@ -27,7 +32,7 @@ Y <- as.matrix(ca[,indx.good[c(2:9,11:17)]])
 #X <- as.integer(as.factor(rbind(c("H","H","H","C","C","M","M","M",    "H","H","H","C","C","M","M"), c(rep("U",8), rep("PI",7)))))
 
 X <- rbind(c(1,1,1,2,2,3,3,3,    1,1,1,2,2,3,3), c(rep(4,8), rep(5,7)))
-ctrl <- ca$type == "protein_coding" ## Assume no changes in protein-coding...
+### ctrl <- ca$type == "protein_coding" ## Assume no changes in protein-coding...
 
 norm_vals <- RUVinv(Y, X, ctrl)
 
