1f2e938b30bca05f0410b7ced98499dc16da5556
Charles Danko <dankoc@gmail.com>
2014-09-28 23:41:40 -0400

    More updates to PCA.R

--- annotations/pca.R
+++ annotations/pca.R
@@ -13,15 +13,15 @@ rpkm_df <- as.matrix(ca[,indx.good[c(2:9,11:17)]]) ## "Good?!"  Remove H2-U, H3-
 for(i in 1:NCOL(rpkm_df)) rpkm_df[,i] <- log(1000*(rpkm_df[,i]+1)/sum(rpkm_df[,i]) *1000/(ca[,"mapSize"]))
 #for(i in 1:NCOL(rpkm_df)) rpkm_df[,i] <- 1000*rpkm_df[,i]/sum(rpkm_df[,i]) *1000/(ca[,"mapSize"])
 
-pca <- prcomp(rpkm_df[,1:8])#, center=TRUE, scale=TRUE) ## UNT
-cols <- c(rep("red",3), rep("green",2), rep("blue", 3))
+pca <- prcomp(rpkm_df[,1:8], center=FALSE, scale=FALSE) ## UNT
+#pca <- prcomp(rpkm_df, scale=FALSE, center=FALSE) ## ALL
 
-pca <- prcomp(rpkm_df)#, scale=T) ## ALL
-cols <- c(rep("red",3), rep("green",2), rep("blue", 3), rep("red", 3), rep("green", 2), rep("blue", 2))
+cols <- c(rep("red",3), rep("green",2), rep("blue", 3), rep("dark red", 3), rep("dark green", 2), rep("dark blue", 2), "black", "black")
+pch <- c(rep(19,8), rep(6,7), 9, 24)
 
 summary(pca) # Prints variance summary for all principal components.
 #plot(pca$rotation[,1], pca$rotation[,2], pch=19, col=cols)
-pairs(pca$rotation[,1:4], col=cols)
+pairs(pca$rotation[,1:5], col=cols, pch=pch)
 
 ## 
 ## Correlate each PC w/ the expected effects of cell compositio on GE.
@@ -46,14 +46,18 @@ CD14c <- bedQuery.bigWig(ca[,1:3], cd14p1, gapValue=0)+ #+bed.region.bpQuery.big
 ctrl_m <- (log((CD4c+1)/sum(CD4c))-log((CD14c+1)/sum(CD14c))) < -5
 ctrl_t <- (log((CD4c+1)/sum(CD4c))-log((CD14c+1)/sum(CD14c))) > 5
 
-monocyte <- (log((CD4c+1)/sum(CD4c))-log((CD14c+1)/sum(CD14c)))
+monocyte <- (log(1000* (CD14c+1)/sum(CD14c)* 1000/(ca[,3]-ca[,2]))) ## Poor approximation for RPKM.
+cd4rnah  <- (log(1000* (CD4c+1)/sum(CD4c)* 1000/(ca[,3]-ca[,2])))
 
 tst <- ctrl_m | ctrl_t
 
 ##
 ## Do correlation.
 for(i in 1:5) {
- print(cor.test(pca$x[tst,i], monocyte[tst], method="spearman"))
-# boxplot(pca$x[ctrl_m,i], pca$x[ctrl_t,i])
+ print(cor.test(pca$x[tst,i], (monocyte-cd4rnah)[tst], method="spearman"))
+ boxplot(pca$x[ctrl_m,i], pca$x[ctrl_t,i])
 }
+
+## Actually include monocyte.
+pca <- prcomp(cbind(rpkm_df, monocyte, cd4rnah), scale=FALSE, center=FALSE) ## ALL
 
