b16b5af42475423528bfcbb4b1714ef37c3ab29d
Charles <dankoc@gmail.com>
2015-05-08 08:51:11 -0400

    Fixed problems arising in annotations pipeline during transition to new server

--- annotations/getCounts.R
+++ annotations/getCounts.R
@@ -18,8 +18,18 @@ bed <- read.table(bed_file)
 bw_plus <- load.bigWig(bw_plus_file)
 bw_minus<- load.bigWig(bw_minus_file)
 
-counts <- bed6.region.bpQuery.bigWig(bw_plus, bw_minus, bed, abs.value=TRUE) #bedQuery.bigWig(bed, bw_plus, bw_minus, gapValue=0)
+## Check if the bigWig has chromosome Y ... if not, remove it before calling.
+if(sum(c(bw_plus$chroms == "chrY"))<=0) {
+  print("WARNING: Skipping chrY.")
+  chY_indx <- bed[,1] == "chrY"
+  counts <- rep(0, NROW(bed))
+  counts[!chY_indx] <- bed6.region.bpQuery.bigWig(bw_plus, bw_minus, bed[!chY_indx,], abs.value=TRUE) #bedQuery.bigWig(bed, bw_plus, bw_minus, gapValue=0)
+  
+} else {
+  counts <- bed6.region.bpQuery.bigWig(bw_plus, bw_minus, bed, abs.value=TRUE) #bedQuery.bigWig(bed, bw_plus, bw_minus, gapValue=0)
+}
 #counts <- abs(counts)  ## Reverse strand for minus.
 
 bed[,5] <- counts
 write.table(bed, out_file, sep="\t", col.names=FALSE, row.names=FALSE, quote=FALSE)
+
