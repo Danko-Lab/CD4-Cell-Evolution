5e5691fc461fb65c103a3856aca67cff60e66a40
Charles <dankoc@gmail.com>
2015-05-30 22:37:01 -0400

    Now lifting combined bigWigs over.

--- enhancer/getSiteStats.R
+++ enhancer/getSiteStats.R
@@ -2,8 +2,8 @@
 load("../annotations/fdr.RData")
 
 tss_aln <- fdr_df[grepl("dREG", ca$annot_type),]
-tss <- read.table("counttss.tsv.tmp")
-tss <- data.frame(tss[,1:20], tss_aln[match(tss$V4, tss_aln$name),31:42])
+tss <- read.table("tss.tsv")
+tss <- data.frame(tss[,c(1:20,44)], tss_aln[match(tss$V4, tss_aln$name),31:42])
 
 ## Alignable fraction (V20) denotes a gap in either species.  Make sure gaps are in both.
 
@@ -19,14 +19,68 @@ tss$V19 <- class
 
 ## Count types of elements ...
 total <- summary(as.factor(tss$V19))
-unmap <- summary(as.factor(tss$V19[tss$V20 == 0]))
-chang <- summary(as.factor(tss$V19[tss$V20 > 0 & tss$fdr_min < 0.01]))
+one2m <- summary(as.factor(tss$V19[tss$V44 > 0])) ## Possible 1:many orthology
+unmap <- summary(as.factor(tss$V19[tss$V44 == 0 & tss$V20 == 0])) ## INDEL
+chang <- summary(as.factor(tss$V19[tss$V44 == 0 & tss$V20 > 0 & tss$fdr_min < 0.05])) # 'High-confidence'
+lccng <- summary(as.factor(tss$V19[(tss$V7 < 0.1 | tss$V8 < 0.1 | tss$V9 < 0.1) & tss$V44 == 0 & tss$V20 > 0 & tss$fdr_min >= 0.05])) # 'Low-confidence'
 
 total
+one2m
 unmap
-chang 
+chang
+lccng 
 
-unmap/total
-chang/total
+one2m/total ## Seems strange that there's such an enrichment in genes here ...
+unmap/(total - one2m)
+chang/(total - one2m)
+lccng/(total - one2m)
 
-## HOWEVER! Note that these numbers seem to include 1:many orthologies, not just gaps.  These really should be removed.
+
+## Create a barplot.
+dat <- data.frame(type= names(unmap), gap= unmap/(total - one2m), change= chang/(total - one2m), lc_change= lccng/(total - one2m), same=(total - one2m - unmap - chang - lccng)/(total - one2m))
+dat <- melt(dat)
+
+require(ggplot2)
+library(reshape2)
+
+a <-  ggplot(dat, aes(x=variable, y=value)) + xlab("") + ylab("Fraction of Transcripts Changed") +
+        geom_bar(aes(fill=type), stat="identity",position=position_dodge(), colour="black") + scale_fill_brewer(palette = "Set3")
+
+b <-  ggplot(dat, aes(x=type, y=value)) + xlab("") + ylab("Fraction of Transcripts Changed") +
+        geom_bar(aes(fill=variable), stat="identity",position=position_dodge(), colour="black") + scale_fill_brewer(palette = "Set3")
+
+c <-  qplot(x= factor(type), y= value, stat="identity", data= dat, geom="bar", fill=factor(variable))
+
+
+pdf("enh.type.change.barplot.pdf")
+	a
+	b
+	c
+dev.off()
+
+
+## Density scatterplots at promoters and enhancers
+
+for(i in 7:12) tss[is.na(tss[,i]),i] <- 0 ## Assign 'NA' scores to 0.
+source("../lib/densScatterplot.R")
+
+max_hc <- rowMax(tss[,c(7,8)])
+densScatterplot(tss$V7, tss$V8, xlab="Human Unt.", ylab="Chimpanzee Unt.", main="dREG Scores")
+densScatterplot(tss$V7[tss$V19 == "Prox_Stab" & max_hc>0.7], tss$V8[tss$V19 == "Prox_Stab" & max_hc>0.7], xlab="Human Unt.", ylab="Chimpanzee Unt.", main="dREG Scores")
+densScatterplot(tss$V7[tss$V19 == "Dist_UnSt" & max_hc>0.7], tss$V8[tss$V19 == "Dist_UnSt" & max_hc>0.7], xlab="Human Unt.", ylab="Chimpanzee Unt.", main="dREG Scores")
+
+max_hm <- rowMax(tss[,c(7,9)])
+densScatterplot(tss$V7[max_hm>0.7], tss$V9[max_hm>0.7], xlab="Human Unt.", ylab="Rhesus Macaque Unt.", main="dREG Scores")
+densScatterplot(tss$V7[tss$V19 == "Prox_Stab" & max_hm>0.7], tss$V9[tss$V19 == "Prox_Stab" & max_hm>0.7], xlab="Human Unt.", ylab="Rhesus Macaque Unt.", main="dREG Scores")
+densScatterplot(tss$V7[tss$V19 == "Dist_UnSt" & max_hm>0.7], tss$V9[tss$V19 == "Dist_UnSt" & max_hm>0.7], xlab="Human Unt.", ylab="Rhesus Macaque Unt.", main="dREG Scores")
+
+max_up <- rowMax(tss[,c(7,10)])
+densScatterplot(tss$V7, tss$V10, xlab="Human Unt.", ylab="Human PI", main="dREG Scores")
+densScatterplot(tss$V7[tss$V19 == "Prox_Stab" & max_up>0.7], tss$V10[tss$V19 == "Prox_Stab" & max_up>0.7], xlab="Human Unt.", ylab="Human PI", main="dREG Scores")
+densScatterplot(tss$V7[tss$V19 == "Dist_UnSt" & max_up>0.7], tss$V10[tss$V19 == "Dist_UnSt" & max_up>0.7], xlab="Human Unt.", ylab="Human PI", main="dREG Scores")
+
+## Compare distance to conservation...
+fact <- cut(tss$V13,c(-Inf,1,5,10,20,50,100,Inf)*1000)
+sapply(fact, function(i) { sum(tss$HumanFDR[fact == i] < 0.01)/sum(fact == i) })
+boxplot()
+
