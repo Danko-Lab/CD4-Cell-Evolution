6e7d9802e1e74cc5acf4266d6bae21df36fff022
Charles <dankoc@gmail.com>
2016-04-08 15:12:05 -0400

    Added avg.metaprofile.R to lib

--- dna_sequence/plotHammingDistance.R
+++ dna_sequence/plotHammingDistance.R
@@ -1,60 +1,83 @@
 require(bigWig)
 
-doplot <- function(bw_file, bed_file, bed_file_conserved, stp=100, halfWindow=10000, ...) {
+doplot <- function(bw_file, bed_file_conserved, bed_file_gain, bed_file_loss, stp=100, halfWindow=10000, ...) {
  bw <- load.bigWig(bw_file)
- bed <- read.table(bed_file)
+ bedg <- read.table(bed_file_gain)
+ bedl <- read.table(bed_file_loss)
  bed_conserved <- read.table(bed_file_conserved)
 
-# bed <- bed[bed[,1]=="chr1",]; bed_conserved <- bed_conserved[bed_conserved[,1]=="chr1",]
-
- meta_change <- metaprofile.bigWig(center.bed(bed, halfWindow, halfWindow), bw, step=stp)
+ meta_g <- metaprofile.bigWig(center.bed(bedg, halfWindow, halfWindow), bw, step=stp)
+ meta_l <- metaprofile.bigWig(center.bed(bedl, halfWindow, halfWindow), bw, step=stp)
  meta_conserved <- metaprofile.bigWig(center.bed(bed_conserved, halfWindow, halfWindow), bw, step=stp)
 
-# plot.metaprofile(meta_change, X0=halfWindow/stp, ...)
+ signal_g <- meta_g$middle
+ signal_l <- meta_l$middle
+ signal_conserved <- meta_conserved$middle
 
- signal_change <- meta_change$middle#/ NROW(meta_change)
- signal_conserved <- meta_conserved$middle#/ NROW(meta_conserved)
+ ylim=c(min(c(signal_g, signal_l, signal_conserved)), max(c(signal_g, signal_l, signal_conserved)))
 
- ylim=c(min(c(signal_change, signal_conserved)), max(c(signal_change, signal_conserved)))
-
- N = length(signal_change)
+ N = length(signal_conserved)
  x = ((1:N) - N/2)* stp #1:N*stp
 
  plot(-500, -500, ylim=ylim, xlim=c(min(x), max(x)), xlab= "Distance [bp]", ylab= "Signal", ...) ## ylab= "Divergences/ 100 bp"
- lines(x, signal_change, col="dark red")
+ lines(x, signal_g, col="dark red")
+ lines(x, signal_l, col="dark blue")
  lines(x, signal_conserved, col="dark green")
 
 }
 
+
+tfbsdoplot <- function(bw, tf_name) {
+  doplot(bw, paste(tf_name,".Hgain-loss.TFBS.bed.gz", sep=""), paste(tf_name,".conserved.TFBS.bed.gz", sep=""), halfWindow=50, stp=1, main=tf_name)
+}
+
+pdf("TFBS.pdf")
+doplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "conserved.TFBS.bed.gz", "Hgain.TFBS.bed.gz", "Hloss.TFBS.bed.gz", halfWindow= 100, stp=1, main="All TFs, 100-way")
+doplot("/local/storage/data/hg19/all/phylopprimate/phyloP46way.primate.bw", "conserved.TFBS.bed.gz", "Hgain.TFBS.bed.gz", "Hloss.TFBS.bed.gz", halfWindow= 100, stp=1, main="All TFs, Primate PhyloP")
+
+tfbsdoplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "ELF1")
+tfbsdoplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "GABPA")
+tfbsdoplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "ARNT")
+tfbsdoplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "YY1")
+tfbsdoplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "MYCN")
+tfbsdoplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "STAT2")
+tfbsdoplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "CREB")
+
+dev.off()
+
+
+
 pdf("PhyloP_100way.pdf")
 
-doplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "H-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="PhyloP at Human LS dREG sites")
-doplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "C-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz")
-doplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "M-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz")
+doplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "conserved.dREG_HD.bed.gz", "H-U.gain.dREG_HD.bed.gz", "H-U.loss.dREG_HD.bed.gz", main="PhyloP at Human LS dREG sites")
+doplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "conserved-distal.dREG_HD.bed.gz", "H-U.gain.dREG_HD.bed.gz", "H-U.loss.dREG_HD.bed.gz", main="PhyloP at Human LS dREG sites")
+
+#doplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "H-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="PhyloP at Human LS dREG sites")
+#doplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "C-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz")
+#doplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "M-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz")
 
 dev.off()
 
 pdf("PrimatePhyloP.pdf")
 
-doplot("/local/storage/data/hg19/all/phylopprimate/phyloP46way.primate.bw", "H-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="PhyloP at Human LS dREG sites")
-doplot("/local/storage/data/hg19/all/phylopprimate/phyloP46way.primate.bw", "C-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="PhyloP at Chimp LS dREG sites")
-doplot("/local/storage/data/hg19/all/phylopprimate/phyloP46way.primate.bw", "M-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="PhyloP at R.Macaque LS dREG sites")
+doplot("/local/storage/data/hg19/all/phylopprimate/phyloP46way.primate.bw", "conserved.dREG_HD.bed.gz", "H-U.gain.dREG_HD.bed.gz", "H-U.loss.dREG_HD.bed.gz", main="PhyloP at Human LS dREG sites")
+doplot("/local/storage/data/hg19/all/phylopprimate/phyloP46way.primate.bw", "conserved-distal.dREG_HD.bed.gz", "H-U.gain.dREG_HD.bed.gz", "H-U.loss.dREG_HD.bed.gz", main="PhyloP at Human LS dREG sites")
+
+#doplot("/local/storage/data/hg19/all/phylopprimate/phyloP46way.primate.bw", "H-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="PhyloP at Human LS dREG sites")
+#doplot("/local/storage/data/hg19/all/phylopprimate/phyloP46way.primate.bw", "C-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="PhyloP at Chimp LS dREG sites")
+#doplot("/local/storage/data/hg19/all/phylopprimate/phyloP46way.primate.bw", "M-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="PhyloP at R.Macaque LS dREG sites")
 
 dev.off()
 
 pdf("DNASequence.pdf")
 
+
+
 ## Changes.
-doplot("hs.ls.diff.bigWig", "H-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="Human divergences, Human gains/ losses.") 
-doplot("pt.ls.diff.bigWig", "C-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="Chimp divergences, Chimp gains/ losses.")
-doplot("rm.ls.diff.bigWig", "M-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="R. macaque divergences, R. macaque gains/ losses.")
-
-doplot("rm.ls.diff.bigWig", "H-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="R. macaque divergences, Human gains/ losses.") ## Control.
+#doplot("hs.ls.diff.bigWig", "H-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="Human divergences, Human gains/ losses.") 
+#doplot("pt.ls.diff.bigWig", "C-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="Chimp divergences, Chimp gains/ losses.")
+#doplot("rm.ls.diff.bigWig", "M-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="R. macaque divergences, R. macaque gains/ losses.")
+#doplot("rm.ls.diff.bigWig", "H-U.gain-loss.dREG_HD.bed.gz", "conserved.dREG_HD.bed.gz", main="R. macaque divergences, Human gains/ losses.") ## Control.
 
 dev.off()
 
-
-pdf("TFBS.pdf")
-doplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "Hgain-loss.TFBS.bed.gz", "conserved.TFBS.bed.gz", halfWindow= 100, stp=3, main="100-way")
-doplot("/local/storage/data/hg19/all/phyloP100way/hg19.100way.phyloP100way.bw", "Hgain-loss.TFBS.bed.gz", "conserved.TFBS.bed.gz", halfWindow=100, stp=3, main="Primate PhyloP")
-dev.off()
---
+++ lib/avg.metaprofile.R
@@ -0,0 +1,48 @@
+## Alternative to Andre's bigWig metaProfile functions that 
+
+
+
+#
+# Convinience function
+#
+
+# Profile object structure:
+# . name
+# . x0 (in units of number of steps)
+# . step (size in bp)
+# . top, middle, bottom vectors
+#
+
+avg.metaprofile.bigWig <- function(bed, bw.plus, bw.minus = NULL, step = 1, name = "Signal", matrix.op = NULL, profile.op = subsampled.quantiles.metaprofile, ...) {
+  #
+  # 1. collect data
+  N = dim(bed)[2]
+  mat = NULL
+  if (N >= 6) {
+    stopifnot(bw.minus != NULL)
+    mat = bed6.step.bpQuery.bigWig(bw.plus, bw.minus, bed, step, op = "avg", abs.value = TRUE, as.matrix = TRUE, follow.strand = TRUE)
+  } else {
+    if (!is.null(bw.minus))
+      warning("bw.minus != NULL but BED contains no strand information")
+    mat = bed.step.bpQuery.bigWig(bw.plus, bed, step, op = "avg", as.matrix = TRUE)
+  }
+  
+  #
+  # 2. apply matrix transformation
+  if (!is.null(matrix.op))
+    mat = matrix.op(mat, ...)
+  
+  #
+  # 3. apply profile operation
+  result = profile.op(mat, ...)
+  
+  #
+  # 4. create result
+  X0 = 0 # can't really tell what X0 was from the input arguments
+  
+  res = c(list(name = name, X0 = X0), result)
+  
+  class(res) <- "metaprofile"
+  return(res)
+}
+
