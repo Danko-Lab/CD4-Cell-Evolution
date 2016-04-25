a24a8dc9c297e2ba6b86f3a0d193cdb419dd8ab8
Charles <dankoc@gmail.com>
2016-04-25 17:01:06 -0400

    Updated code to compare enhancers that loop to conserved promoters.

--- enhancer/enhancer_conservation_at_conserved_genes.R
+++ enhancer/enhancer_conservation_at_conserved_genes.R
@@ -26,6 +26,7 @@ load("../annotations/fdr.RData")
 ## Get enhancers interacting with conserved promoters...
 indx1TRE <- integer(0)
 indxGT1TRE<-integer(0)
+loopsToEnh<-data.frame(nloops=integer(), nenh=integer())
 
  ## Get TSS of conserved, annotated protein coding genes.
  indx <- fdr_df$fdr_min > 0.15 & ca$type == "protein_coding" & abs(fdr_df$fc_min) < 0.75
@@ -36,39 +37,53 @@ indxGT1TRE<-integer(0)
  ## dREG sites.
  tre_aln <- fdr_df[grepl("dREG", ca$annot_type),]
  tre <- read.table("tss.tsv")
- tres <- data.frame(tre, tre_aln[match(tre$V4, tre_aln$name),c(9,33:50)])
+ tres <- data.frame(tre, tre_aln[match(tre$V4, tre_aln$name),c(9,33:50,7:8,10)])
  tres$V7[is.na(tres$V7)] = 0; tres$V8[is.na(tres$V8)] = 0; tres$V9[is.na(tres$V9)] = 0
  
  ## Find out which loops intersect.
  for(i in c(1:NROW(tss))) {
   if((i %% 100) == 0) print(i)
+
+  ## If it finds a TSS ...
   indx1 <- getOverlap(tss[i,], loops1)
   indx2 <- getOverlap(tss[i,], loops2)
   nloops <- NROW(indx1) + NROW(indx2)
 
+  ## ... then compare with loops on the other end.
   ## Compare 1 loop ...
+  enhLoops <- integer(0)
   if(nloops == 1) {
     if(NROW(indx1)>0) {
       indx1TRE <- c(indx1TRE, getOverlap(loops2[indx1,], tres))
+      enhLoops <- c(enhLoops, getOverlap(loops2[indx1,], tres))
     }
     if(NROW(indx2)>0) {
       indx1TRE <- c(indx1TRE, getOverlap(loops1[indx2,], tres))
+      enhLoops <- c(enhLoops, getOverlap(loops1[indx2,], tres))
     }
   } 
 
-  ## ... to more than 1 loop.
+  ## ... to more than 2 loops.
   if(nloops > 2) {
     if(NROW(indx1)>0) {
       indxGT1TRE <- c(indxGT1TRE, getOverlap(loops2[indx1,], tres))
+      enhLoops <- c(enhLoops, getOverlap(loops2[indx1,], tres))
     }
     if(NROW(indx2)>0) {
       indxGT1TRE <- c(indxGT1TRE, getOverlap(loops1[indx2,], tres))
+      enhLoops <- c(enhLoops, getOverlap(loops1[indx2,], tres))
     }
   }
+
+ loopsToEnh <- rbind(loopsToEnh, data.frame(nloops, NROW(enhLoops)))
+ # if(nloops > 0) print(paste(nloops, NROW(enhLoops)))
+
  }
 
 indx1TRE <- unique(sort(indx1TRE)); NROW(indx1TRE)
 indxGT1TRE <- unique(sort(indxGT1TRE)); NROW(indxGT1TRE)
+
+plot(loopsToEnh)
 
 #########
 ## Do enhancers that loop to exactly 1 TRE change less frequently?
